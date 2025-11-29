c1909effc65abeaf4a495c8ef478e67edb4ae55d
incamen <ic21062025@gmail.com>
2025-11-29 19:24:04 +0700

    Update generate_config_from_theme.py

--- scripts/generate_config_from_theme.py
+++ scripts/generate_config_from_theme.py
@@ -9,7 +9,7 @@ THEME_PATH = os.path.join(ROOT_DIR, "config", "next_theme.txt")
 CONFIG_PATH = os.path.join(ROOT_DIR, "config", "next_article.json")
 
 SPACE_NAME = "Penerang/Teuing-Ah"
-API_NAME = "/api_generate"  # nama endpoint di Space
+API_NAME = "/generate_config"  # nama endpoint di Space
 
 
 def load_theme_text():
